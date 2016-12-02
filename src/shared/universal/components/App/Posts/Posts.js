c4deb6f748577f286504f42b76096bb46857cd0f
mskec <martin.skec@auto1.com>
2016-12-02 12:07:28 +0100

    Posts log

--- src/shared/universal/components/App/Posts/Posts.js
+++ src/shared/universal/components/App/Posts/Posts.js
@@ -1,25 +1,32 @@
 /* @flow */
 
-import React from 'react';
+import React, { Component } from 'react';
 import { Link, Match } from 'react-router';
 import Helmet from 'react-helmet';
 import Post from './Post';
 
-function Posts() {
-  return (
-    <div>
-      <Helmet title="Posts" />
 
-      <h1>Posts</h1>
+class Posts extends Component {
+  componentDidMount() {
+    console.log('Posts.mount');
+  }
 
-      <ul>
-        <li><Link to="/posts/1">Post 1</Link></li>
-        <li><Link to="/posts/2">Post 2</Link></li>
-      </ul>
+  render() {
+    return (
+      <div>
+        <Helmet title="Posts" />
 
-      <Match pattern="/posts/:id" component={Post} />
-    </div>
-  );
+        <h1>Posts</h1>
+
+        <ul>
+          <li><Link to="/posts/1">Post 1</Link></li>
+          <li><Link to="/posts/2">Post 2</Link></li>
+        </ul>
+
+        <Match pattern="/posts/:id" component={Post} />
+      </div>
+    );
+  }
 }
 
 export default Posts;
